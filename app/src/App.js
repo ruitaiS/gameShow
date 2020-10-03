29996edf6767eb7dc1f837f92b27c8ad3666822c
ruitaiS <shaoruitai@gmail.com>
2020-10-03 13:51:59 -0400

    drew a line

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 // import './App.css';
 import GameShow from './Components/GameShow'
+import{
+    Line,
+    SteppedLine,
+    PolyLine,
+    Circle,
+    Rectangle
+} from 'draw-shape-reactjs';
 
 function App() {
   return (
@@ -8,6 +15,14 @@ function App() {
         <p>
           Game Show App!
         </p>
+
+        <Line
+            position='fixed'
+            from={[110, 610]}
+            to={[600, 850]}
+            color='#1DBFE7'
+        />
+
         <GameShow/>
     </div>
   );
