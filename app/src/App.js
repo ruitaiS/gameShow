07e52360b3c202a2133ae5e57da93ff8a3994be8
RonanAlmeida <ronancalmeida@hotmail.com>
2020-10-03 12:07:33 -0400

    wrench folder

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GameShow from './Components/GameShow'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Game Show App Testing!
+          Game Show App!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <GameShow/>
     </div>
   );
 }
---
+++ app/src/Components/GameShow.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function GameShow() {
+  return <div > testing  this is where wrench will be used </div>;
+}
+export default GameShow;
