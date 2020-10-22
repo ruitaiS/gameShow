440dfb61f8fc6e1d6c28ddc11a766e9e19e66d85
ruitaiS <shaoruitai@gmail.com>
2020-10-22 00:17:16 -0400

    code cleanup; moved styling from ReactCamera into css classes

--- app/src/Components/ReactCamera.js
+++ app/src/Components/ReactCamera.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Webcam from "react-webcam";
+import '../index.css'
 
 function ReactCamera(props){
 
 const videoConstraints = {
         //width: 1280,
         //height: 720,
-
-        //wrnch uses a square
-        //Make sure the width/height are the same as "scale" in drawFigure in App
+        //wrnch renders a square
+        //Make sure width is the same as "scale" in drawFigure in App.js
+        //Vertical alignment will need tweaking if camera input is not also square
         width: 480,
         height: 480,
         facingMode: "user"
@@ -21,42 +22,25 @@ const videoConstraints = {
     const capture = React.useCallback(() => {
         //getScreenshot() is already base64!
         const imageSrc = webcamRef.current.getScreenshot();
-        console.log(imageSrc);
-
         //Call sendToFlask method from within App.js
         props.sendToFlask(imageSrc);
-
         setImgSrc(imageSrc);
-
     }, [webcamRef, setImgSrc]);
 
-
-        return(
-            <div>
-                <head>
-                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossOrigin="anonymous"/>
-                </head>
-
-                <div style={{position: 'absolute', left: '50%', top: '30%',transform: 'translate(-50%, -50%)'}}>
-                
-                <Webcam
+    return(
+        <div>
+            <Webcam
+                class="webcam"
                 videoConstraints={videoConstraints}
-
-
                 audio={false}
                 ref={webcamRef}
                 screenshotFormat="image/jpeg"
-                />
+            />
 
-                </div>
-                <div style={{position: 'absolute', left: '50%', top: '70%', transform: 'translate(-50%, -50%)'}}> <button onClick={capture}>Capture photo</button> </div>
-                {imgSrc && (
-                <img
-                src={imgSrc}
-                />
-                )}
-            </div>
-        );
+            <button class = "webcamBtn" onClick={capture}>Capture Photo</button>
+            <img src={imgSrc}/>
+        </div>
+    );
 }
 
 export default ReactCamera;
